45a3bda83ec35254fc0b2e73645c14d16099fc73
Ferdyansyah Permana Putra <96618837+FerdyPut@users.noreply.github.com>
2025-09-22 08:59:22 +0700

    Update dash.py

--- dash.py
+++ dash.py
@@ -225,7 +225,7 @@ fig_barh = go.Figure(go.Bar(
     text=[f"{v:.2f}" for v in top5_stocks.values],
     textposition='auto',
     orientation='h',           # ini bikin horizontal
-    marker=dict(color='mediumslateblue')
+    marker=dict(color='#091a30')
 ))
 
 fig_barh.update_layout(
