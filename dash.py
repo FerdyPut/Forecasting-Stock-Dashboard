bc216829a965b3983d6b2ff852e4ea96cb3e1643
Ferdyansyah Permana Putra <96618837+FerdyPut@users.noreply.github.com>
2025-09-22 15:54:37 +0700

    Update dash.py

--- dash.py
+++ dash.py
@@ -248,7 +248,7 @@ with col2:
                         delta="\u200B IDR, USD, etc.",
                         delta_color = "off"
                     )
-                    st.badge(f"Nilai dalam satuan Mata Uang terkait", color="#cd4d4d")
+                    st.badge(f"Nilai dalam satuan Mata Uang terkait", color= "orange")
 
             with col2_s:
                 with st.container(border=True):
